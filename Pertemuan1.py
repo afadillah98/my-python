624472a565e42826c66f1fda81ca79338d8df32e
Arif Fadillah <afadillah876@gmail.com>
2023-02-21 13:32:07 +0700

    Python Pertama

---
+++ Pertemuan1.py
@@ -0,0 +1,20 @@
+x = "python"
+y = "is"
+z = "awesome"
+
+print(x + y + z)
+
+print(" ")
+
+x = 5
+y = "john"
+print(x, y)
+
+x = "awesome"
+def myfunc():
+    print("Python is " + x)
+
+myfunc()
+
+
+print(x.count(x))
