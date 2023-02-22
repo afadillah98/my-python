82a9b743f0c74797e79049d73b310f9b2764f900
Arif Fadillah <afadillah876@gmail.com>
2023-02-22 10:24:25 +0700

    Menambahkan list dan memodifikasinya

--- Pertemuan1.py
+++ Pertemuan1.py
@@ -18,3 +18,24 @@ myfunc()
 
 
 print(x.count(x))
+
+
+#List
+mylist = ["apple", "banana", "mango", "apple"]
+print(len(mylist))
+print(type(mylist))
+print(mylist[1])
+
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[2:5])
+
+if "mango" in thislist:
+    print("Yes, Manggo is in the Fruits list")
+    
+# Mengubah nilai list
+thislist[1] = "Rambutan"
+print(thislist[1])
+thislist.insert(1, "Leci")
+thislist.remove("orange")
+for x in thislist:
+    print(x)
